063cc0b3752f9e4f38cce13939ded6a134d03644
moatasem1234 <matsmbllah1234@gmail.com>
2025-05-24 23:56:31 +0300

    add auth controller and reSort migration files

--- database/migrations/0001_01_01_000002_create_roles_and_permissions_table.php
+++ database/migrations/0001_01_01_000002_create_roles_and_permissions_table.php
@@ -10,20 +10,6 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('gender', ['male', 'female'])->nullable();
-            $table->date('birthday')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('address')->nullable();
-            $table->string('image')->nullable();
-            $table->rememberToken();
-            $table->timestamps();
-        });
 
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
@@ -95,7 +81,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
         Schema::dropIfExists('roles');
         Schema::dropIfExists('permissions');
         Schema::dropIfExists('role_user');
