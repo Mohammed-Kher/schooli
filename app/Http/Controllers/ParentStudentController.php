378774d772c08213f2d119986516c280a13f28f7
moatasem1234 <matsmbllah1234@gmail.com>
2025-06-02 19:27:23 +0300

    asdasdasdas asddasdsadasad

--- app/Http/Controllers/ParentStudentController.php
+++ app/Http/Controllers/ParentStudentController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\ParentStudent;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class ParentStudentController extends Controller
 {
@@ -26,10 +29,27 @@ class ParentStudentController extends Controller
         if (!Auth::user()->hasPermission('manage-parent_students')) {
             return response()->json(['error' => 'Unauthorized'], 403);
         }
+
         $validatedData = $request->validate([
-            'user_id' => ['required', 'exists:users,id'],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'],
         ]);
-        $parent = ParentStudent::create($validatedData);
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make(($validatedData['password'])),
+        ]);
+
+        $role = DB::table('roles')->where('slug', 'parent')->first();
+
+        DB::table('role_user')->insert([
+            'user_id' => $user->id,
+            'role_id' => $role->id,
+        ]);
+
+
+        $parent = ParentStudent::create(['user_id' => $user->id]);
         return response()->json([
             'data' => $parent->toArray(),
             'status' => self::HTTP_OK,
@@ -50,17 +70,43 @@ class ParentStudentController extends Controller
         ]);
     }
 
-    public function update(Request $request, ParentStudent $parentStudent)
+    public function update(Request $request, string $id)
     {
+
+        // dd($parentStudent);
         if (!Auth::user()->hasPermission('manage-parent_students')) {
             return response()->json(['error' => 'Unauthorized'], 403);
         }
+        // if(!$parentStudent) {
+        //     return response()->json(['error' => 'ParentStudent not found'], 404);
+        // }
+        // if(!$parentStudent->user) {
+        //     return response()->json(['error' => 'User not found for this ParentStudent'], 404);
+        // }
+
         $validatedData = $request->validate([
-            'user_id' => ['sometimes', 'exists:users,id'],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['nullable', 'string', 'min:8'],
         ]);
-        $parentStudent->update($validatedData);
+        $user = User::find($id);
+        // dd($user);
+
+        if ($request->input('password')) {
+            $user->update([
+                'name' => $validatedData['name'] ?? $user->name,
+                'email' => $validatedData['email'] ?? $user->email,
+                'password' => Hash::make($validatedData['password']) ?? $user->password,
+            ]);
+        } else {
+            $user->update([
+                'name' => $validatedData['name'] ?? $user->name,
+                'email' => $validatedData['email'] ?? $user->email,
+            ]);
+        }
+        // dd($user);
         return response()->json([
-            'data' => $parentStudent->toArray(),
+            'data' => $user,
             'status' => self::HTTP_OK,
             'message' => self::UPDATED,
         ]);
--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -65,8 +65,7 @@ class RolePermissionSeeder extends Seeder
             ['name' => 'إدارة المعلمين', 'slug' => 'manage-teachers', 'description' => 'إنشاء وتحديث وحذف المعلمين'],
             ['name' => 'عرض الحضور', 'slug' => 'view-attendance', 'description' => 'عرض سجلات الحضور'],
             ['name' => 'إدارة الحضور', 'slug' => 'manage-attendance', 'description' => 'إنشاء وتحديث وحذف سجلات الحضور'],
-            ['name' => 'إدارة الآباء', 'slug' => 'manage-parent_students', 'description' => 'إدارة الآباء'],
-            ['name' => 'عرض الآباء', 'slug' => 'view-parent_students', 'description' => 'عرض الآباء'],
+
         ];
 
         foreach ($permissions as $permission) {
@@ -100,6 +99,7 @@ class RolePermissionSeeder extends Seeder
                 'view-days',
                 'view-events',
                 'create-events',
+                'manage-students',
                 'edit-events',
                 'delete-events',
                 'view-homework',
